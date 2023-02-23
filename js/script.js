9f25ded37bc94925c2a050f9a8605cbe45d60cc2
Amal-anil <amalanilmkm@gmail.com>
2023-02-23 14:21:44 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-function scroll() {
-    document.getElementById("nav1").style.backgroundColor = "rgba(0,0,0,.5)";
-}
+// function scroll() {
+//     document.getElementById("nav1").style.backgroundColor = "rgba(0,0,0,.5)";
+// }
 
 function submit1() {
     console.log("hekko");
@@ -15,3 +15,19 @@ function submit1() {
         return true;
     }
 }
+
+// Get the div
+var myDiv = document.getElementById("nav1");
+
+// Listen for scroll event on the window object
+window.addEventListener("scroll", function () {
+    // Get the current scroll position of the window
+    var scrollTop = window.pageYOffset;
+
+    // If the scroll position is greater than 50px, change color of div to red
+    if (scrollTop > 50) {
+        myDiv.style.backgroundColor = "rgba(0,0,0,.5)";
+    } else {
+        myDiv.style.backgroundColor = "";
+    }
+});
